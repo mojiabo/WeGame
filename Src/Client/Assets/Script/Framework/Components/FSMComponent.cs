67f36f16ccf4b12bcb26c65439b52f89a8d7fe90
mojiabo <179066821@qq.com>
2019-04-24 14:39:35 +0800

    通过状态机组件动态创建状态机Fsm

--- Src/Client/Assets/Script/Framework/Components/FSMComponent.cs
+++ Src/Client/Assets/Script/Framework/Components/FSMComponent.cs
@@ -5,10 +5,42 @@ namespace Framework
 {
     public class FSMComponent : BaseComponent
     {
+        private FSMManager m_FSMManager;
+
+        private int m_TemFSMId;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            m_FSMManager = new FSMManager();
+        }
+
+        /// <summary>
+        /// 创建状态机
+        /// </summary>
+        /// <typeparam name="T">拥有者类型</typeparam>
+        /// <param name="fsmId">状态机编号</param>
+        /// <param name="owner">拥有者</param>
+        /// <param name="status">状态数组</param>
+        /// <returns></returns>
+        public FSM<T> CreateFSM<T>(T owner, FSMState<T>[] status) where T : class
+        {
+            return  m_FSMManager.CreateFSM<T>(m_TemFSMId++,owner,status);
+        }
+
+        /// <summary>
+        /// 销毁状态机
+        /// </summary>
+        /// <param name="fsmId"></param>
+        public void DestoryFSM(int fsmId)
+        {
+             m_FSMManager.DestoryFSM(fsmId);
+        }
+
 
         public override void Shutdown()
         {
-
+            m_FSMManager.Dispose();
         }
     }
 }
--- Src/Client/Assets/Script/Framework/Managers/FSM/FSMManager.cs
+++ Src/Client/Assets/Script/Framework/Managers/FSM/FSMManager.cs
@@ -1,11 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace Framework
 {
-    public class FSMManager : ManagerBase
+    public class FSMManager : ManagerBase,IDisposable
     {
+        private Dictionary<int, FSMBase> m_FSMDic;
 
+        public FSMManager()
+        {
+            m_FSMDic = new Dictionary<int, FSMBase>();
+        }
 
+        /// <summary>
+        /// 创建状态机
+        /// </summary>
+        /// <typeparam name="T">拥有者类型</typeparam>
+        /// <param name="fsmId">状态机编号</param>
+        /// <param name="owner">拥有者</param>
+        /// <param name="status">状态数组</param>
+        /// <returns></returns>
+        public FSM<T> CreateFSM<T>(int fsmId,T owner,FSMState<T>[]status)where T:class
+        {
+            FSM<T> fsm = new FSM<T>(fsmId,owner,status);
+            m_FSMDic[fsmId] = fsm;
+            return fsm;
+        }
+
+        /// <summary>
+        /// 销毁状态机
+        /// </summary>
+        /// <param name="fsmId"></param>
+        public void DestoryFSM(int fsmId)
+        {
+            FSMBase fsm = null;
+
+            if (m_FSMDic.TryGetValue(fsmId,out fsm))
+            {
+                fsm.ShutDown();
+                m_FSMDic.Remove(fsmId);
+            }
+
+        }
+
+        public void Dispose()
+        {
+            foreach (KeyValuePair<int, FSMBase> fsm in m_FSMDic)
+            {
+                fsm.Value.ShutDown();
+            }
+            m_FSMDic.Clear();
+        }
     }
 }
